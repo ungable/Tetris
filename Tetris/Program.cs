f7ad0b80e12b804c620a000abbcc4ee9fd7734e1
Evgenii Dobrygin <sarecud@mail.ru>
2022-03-24 02:11:31 +0700

    Вывели 2 точки на экран

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Runtime.Versioning;
 
 namespace Tetris
 {
-    internal class Program
+    class Program
     {
+        [SupportedOSPlatform("windows")]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Drow(x1, y1, c1);
+
+            Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
